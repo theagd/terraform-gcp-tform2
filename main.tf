f1240722f665dca8d2722b070914fb260ce6f413
theagd <50429521+theagd@users.noreply.github.com>
2023-10-04 17:31:12 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,4 +8,11 @@ terraform {
 provider "google" {
   project = var.project
   region = var.region
+resource "google_storage_bucket" "static" {
+ name          = "agd-bucket-3"
+ location      = "US"
+ storage_class = "STANDARD"
+
+ uniform_bucket_level_access = true
+}
 }
